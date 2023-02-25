bfd584bc62c83db578b9207bed83c6fd9a9bf4e7
r-sanjiv <114347326+r-sanjiv@users.noreply.github.com>
2023-02-25 15:22:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 from PIL import Image
 
 st.text("WELCOME")
-image = Image.open('https://github.com/r-sanjiv/ML_Application_Using_Streamlit/blob/1db6ffd3cec5d28b406be9dbb70d93ff8c85ea9f/Image.jpg')
+image = Image.open('Image.jpg')
 st.image(image, caption='')
