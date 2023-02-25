1a95d042d24aaa8c8d86ecb17b2152b5a0652479
r-sanjiv <114347326+r-sanjiv@users.noreply.github.com>
2023-02-25 17:24:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ st.markdown(" - AI-generated inventions.")
 st.image(Image.open("Image_5.jpg"))
 
 profile_pic = Image.open("Profile_pic.jpg")
-st.image(image, caption = '')
+
 st.header("About Us")
 st.image(profile_pic, width=100, use_column_width=None, clamp=False, channels="RGB", caption = "SANJIV.R")
 st.markdown("AI Engineer and Quantam Meachine Learning Engineer")
