b61fcdc0add866d64e777bf50cc81b5eaba335ec
r-sanjiv <114347326+r-sanjiv@users.noreply.github.com>
2023-02-25 17:41:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,6 @@ st.header("About Us")
 st.image(profile_pic, width=100, use_column_width=None, clamp=False, channels="RGB", caption = "SANJIV.R")
 st.markdown("AI Engineer, Quantam Meachine Learning Engineer, Data Analyst, ML Front End and Back End Developer")
 st.markdown("Contact Us")
-st.markdonw("GitHub : "%url1)
-st.markdown("LinedIn : "%url2)
+st.write("GitHub : ",url1)
+st.write("LinedIn : ",url2)
 
