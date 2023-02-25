0bf4075f5ce837253c670cd54e53607e10cf2587
r-sanjiv <114347326+r-sanjiv@users.noreply.github.com>
2023-02-25 15:48:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from PIL import Image
 
-st.text("WELCOME")
+st.title("WELCOME")
 image = Image.open('Image.jpg')
 profile_pic = Image.open("Profile_pic.jpg")
 st.image(image, caption = '')
 st.header("About Us")
-st.text("this web application was developed by...................")
-st.image(profile_pic, caption = "SANJIV.R")
+st.text("this web application was developed by....")
+st.image(profile_pic, width=100, use_column_width=None, clamp=False, channels="RGB", caption = "SANJIV.R")
