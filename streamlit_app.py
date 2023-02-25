6b30022ab3b413508b8c1690a7f35138273deed2
r-sanjiv <114347326+r-sanjiv@users.noreply.github.com>
2023-02-25 15:59:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 from PIL import Image
 
-st.title("WELCOME")
+st.title("ARTIFICIAL INTELLIGENCE")
 image = Image.open('Image.jpg')
+st.header("what is AI ?..")
+st.markdown("Artificial intelligence (AI) involves using computers to do things that traditionally require human intelligence")
+st.subheader("Types fo Artificial Intelligence (AI)")
+st.markdown("Artificial Narrow Intelligence (ANI)")
+st.markdown("Artificial General Intelligence (AGI)")
+st.markdown("Artificial Super Intelligence (ASI)")
 profile_pic = Image.open("Profile_pic.jpg")
 st.image(image, caption = '')
 st.header("About Us")
