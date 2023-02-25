62d29f7a22ac150b744a40503db056c38abef73a
r-sanjiv <114347326+r-sanjiv@users.noreply.github.com>
2023-02-25 15:35:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,8 @@ from PIL import Image
 
 st.text("WELCOME")
 image = Image.open('Image.jpg')
-st.image(image, caption='')
+st.image(image, caption = '')
+set_page_config(layout = "wide")
+Header.text("About Us")
+st.text("this web application was developed by...................")
+            st.image(Image.open("Profile_pic.jpg"), caption = "SANJIV.R")
